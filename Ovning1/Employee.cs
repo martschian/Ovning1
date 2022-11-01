07a04881c28f1fb0c214a7db567d63ca6844b745
Håkan Hellgren <67669+martschian@users.noreply.github.com>
2022-11-01 15:01:09 +0100

    Added class for employees

---
+++ Ovning1/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning1
+{
+    internal class Employee
+
+    {
+        string name;
+        double wage;
+
+        public Employee(string name, double wage)
+        {
+            name = this.name;
+            wage = this.wage;
+        }
+    }
+}
