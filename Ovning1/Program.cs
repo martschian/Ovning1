7616f2ced8f912b29cd2aec3151fe1664e0a0718
Håkan Hellgren <67669+martschian@users.noreply.github.com>
2022-11-01 15:11:19 +0100

    Add menu skeleton

--- Ovning1/Program.cs
+++ Ovning1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string selection = "";
+bool exitProgram = false;
+do
+{
+    Console.WriteLine("Personalregister");
+    Console.WriteLine("================");
+    Console.WriteLine("1. Visa anställda");
+    Console.WriteLine("2. Lägg till ny personal");
+    Console.WriteLine("(A)vsluta programmet\n\n");
+
+    Console.Write("Ditt val: ");
+    selection = Console.ReadLine();
+
+    switch (selection)
+    {
+        case "1":
+            Console.WriteLine("TO-DO: Add employee");
+            break;
+        case "2":
+            Console.WriteLine("TO-DO: Display employees");
+            break;
+        case "a":
+        case "A":
+            Console.WriteLine("Hej då!");
+            exitProgram = true;
+            break;
+        default:
+            Console.WriteLine("Felaktig inmatning");
+            break;
+    }
+
+} while (!exitProgram);
