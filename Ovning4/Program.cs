f6e790dffcce15670cabf38b4a52d0d72557298c
Håkan Hellgren <67669+martschian@users.noreply.github.com>
2022-11-15 10:13:14 +0100

    Add Ovning 4

---
+++ Ovning4/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    class Program
+    {
+        /// <summary>
+        /// The main method, vill handle the menues for the program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
+                    + "\n1. Examine a List"
+                    + "\n2. Examine a Queue"
+                    + "\n3. Examine a Stack"
+                    + "\n4. CheckParanthesis"
+                    + "\n0. Exit the application");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                switch (input)
+                {
+                    case '1':
+                        ExamineList();
+                        break;
+                    case '2':
+                        ExamineQueue();
+                        break;
+                    case '3':
+                        ExamineStack();
+                        break;
+                    case '4':
+                        CheckParanthesis();
+                        break;
+                    /*
+                     * Extend the menu to include the recursive 
+                     * and iterative exercises.
+                     */
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Examines the datastructure List
+        /// </summary>
+        static void ExamineList()
+        {
+            /*
+             * Loop this method untill the user inputs something to exit to main menue.
+             * Create a switch statement with cases '+' and '-'
+             * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
+             * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
+             * In both cases, look at the count and capacity of the list
+             * As a default case, tell them to use only + or -
+             * Below you can see some inspirational code to begin working.
+            */
+
+
+            /***********************************************
+             * 
+             *           SVAR PÅ FRÅGOR ÖVNING 4.1
+             *           
+             ***********************************************
+             * 2. Listans kapacitet ökar när du lägger till ett nytt element till en full lista (Count == Capacity)
+             * 3. Kapaciteten fördubblas (4->8->16 osv)
+             * 4. Därför att utöka kapaciteten är en dyr operation. En ny array måste allokeras och alla element
+             *    från den gamla arrayen kopieras till den nya.
+             * 5. Nej. (Det är möjligt att minska listans kapacitet genom att anropa TrimExcess())
+             * 6. När du vet exakt antal element du kommer att hantera.
+             */
+
+            List<string> theList = new List<string>();
+            string input;
+            char nav;
+            string value;
+            do
+            {
+                Console.WriteLine("+<sträng> för att lägga till strängen i listan. -<sträng> för att ta bort. q för att gå till huvudmenyn.");
+                Console.WriteLine($"Listan håller fn. {theList.Count} element med en kapacitet av {theList.Capacity}");
+                input = Console.ReadLine();
+                nav = input[0];
+                value = input.Substring(1); 
+                switch (nav)
+                {
+                    case '+':
+                        theList.Add(value);
+                        break;
+                    case '-':
+                        theList.Remove(value);
+                        break;
+                    case 'q':
+                        return;
+                    default: 
+                        Console.WriteLine("Använd endast \"+\" eller \"-\"");
+                        break;
+                }
+
+            } while (true);
+            
+
+        }
+
+        /// <summary>
+        /// Examines the datastructure Queue
+        /// </summary>
+        static void ExamineQueue()
+        {
+            /*
+             * Loop this method untill the user inputs something to exit to main menue.
+             * Create a switch with cases to enqueue items or dequeue items
+             * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
+            */
+        }
+
+        /// <summary>
+        /// Examines the datastructure Stack
+        /// </summary>
+        static void ExamineStack()
+        {
+            /*
+             * Loop this method until the user inputs something to exit to main menue.
+             * Create a switch with cases to push or pop items
+             * Make sure to look at the stack after pushing and and poping to see how it behaves
+            */
+        }
+
+        static void CheckParanthesis()
+        {
+            /*
+             * Use this method to check if the paranthesis in a string is Correct or incorrect.
+             * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
+             * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
+             */
+
+        }
+
+    }
+}
