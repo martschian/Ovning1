0541c8db3a1f1272b69f56477d1de58914c4ef99
Håkan Hellgren <67669+martschian@users.noreply.github.com>
2022-11-30 09:33:14 +0100

    Add extension method for generating unique registration numbers

--- Ovning5/Extensions/RandomExtensions.cs
+++ Ovning5/Extensions/RandomExtensions.cs
@@ -1,16 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ovning5.Extensions
+﻿namespace Ovning5.Extensions
 {
     internal static class RandomExtensions
     {
-        public static string RandomString(this Random rnd, int length, string allowedCharacters) 
+        public static string RandomRegistrationNumber(this Random rnd, int length, string allowedCharacters)
         {
-            return "MAG545";
+            ISet<string> usedStrings = new HashSet<string>();
+
+            char[] chars = new char[length];
+            int setLength = allowedCharacters.Length;
+            string randomString;
+
+            do
+            {
+                for (int i = 0; i < length / 2; ++i)
+                {
+                    chars[i] = allowedCharacters[rnd.Next(setLength)];
+                }
+
+                for (int i = length / 2; i < length; ++i)
+                {
+                    chars[i] = Char.Parse(rnd.Next(9).ToString());
+                }
+
+                randomString = new string(chars, 0, length);
+
+                if (usedStrings.Add(randomString))
+                    break;
+            } while (true);
+
+            return randomString;
         }
     }
 }
