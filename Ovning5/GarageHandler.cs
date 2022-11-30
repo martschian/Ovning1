599fe90db1fe70e9c483fa2f2cf5ed6492cdc032
Håkan Hellgren <67669+martschian@users.noreply.github.com>
2022-11-30 09:36:39 +0100

    Add methods for populating the garage with vehicles and listing all vehicles

--- Ovning5/GarageHandler.cs
+++ Ovning5/GarageHandler.cs
@@ -5,14 +5,37 @@ namespace Ovning5
     internal class GarageHandler
     {
         private readonly IUI _ui;
-        public GarageHandler(IUI ui)
+        private Garage<IVehicle> _garage;
+
+        public GarageHandler(IUI ui, Garage<IVehicle> garage)
         {
             _ui = ui;
+            _garage = garage;
         }
 
         public void Init()
         {
             _ui.ShowMainMenu();
+        }
+        public void PopulateGarage(int? numberOfVehicles = null)
+        {
+            numberOfVehicles ??= _garage.Capacity;
+
+            var vehicles = VehicleFactory.GetVehicles((int)numberOfVehicles);
+
+            foreach ( var vehicle in vehicles )
+            {
+                _garage.AddVehicle(vehicle);
+            }
+        }
+
+        public void ListAllVehicles()
+        {
+            foreach (var vehicle in _garage)
+            {
+                Console.WriteLine(vehicle);
+            }
+            
         }
 
     }
