419af6e01acce801dfd4e77d0bc36331fffc50e3
Håkan Hellgren <67669+martschian@users.noreply.github.com>
2022-11-30 15:07:22 +0100

    Improve Factory for more randomness among vehicles

--- Ovning5/VehicleFactory.cs
+++ Ovning5/VehicleFactory.cs
@@ -13,6 +13,7 @@ namespace Ovning5
         internal static List<IVehicle> GetVehicles(int numberOfVehicles = 1)
         {
             var vehicles = new List<IVehicle>();
+            var colors = new string[] { "black", "white", "silver", "red", "blue", "green", "yellow" };
             var rnd = new Random();
 
             for (int i = 0; i < numberOfVehicles; i++)
@@ -20,19 +21,19 @@ namespace Ovning5
                 switch (rnd.Next(1, 6))
                 {
                     case 1:
-                        vehicles.Add(new Car("Blue", "Volvo", "740", rnd.RandomRegistrationNumber(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 4));
+                        vehicles.Add(new Car(colors[rnd.Next(colors.Length)], "Volvo", "740", rnd.RandomRegistrationNumber(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 4));
                         break;
                     case 2:
-                        vehicles.Add(new Airplane("White", "Airbus", "330", rnd.RandomRegistrationNumber(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 20));
+                        vehicles.Add(new Airplane(colors[rnd.Next(colors.Length)], "Airbus", "330", rnd.RandomRegistrationNumber(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 20));
                         break;
                     case 3:
-                        vehicles.Add(new Motorcycle("Black", "Harley-Davidson", "Softtail", rnd.RandomRegistrationNumber(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 2));
+                        vehicles.Add(new Motorcycle(colors[rnd.Next(colors.Length)], "Harley-Davidson", "Softtail", rnd.RandomRegistrationNumber(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 2));
                         break;
                     case 4:
-                        vehicles.Add(new Bus("Red", "Scania", "Citywide", rnd.RandomRegistrationNumber(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 70));
+                        vehicles.Add(new Bus(colors[rnd.Next(colors.Length)], "Scania", "Citywide", rnd.RandomRegistrationNumber(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 70));
                         break;
                     case 5:
-                        vehicles.Add(new Boat("Wood", "Aquafina", "Velvet", rnd.RandomRegistrationNumber(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 1));
+                        vehicles.Add(new Boat(colors[rnd.Next(colors.Length)], "Aquafina", "Velvet", rnd.RandomRegistrationNumber(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 1));
                         break;
                 }
             }
